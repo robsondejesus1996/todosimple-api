489a6f3c120676138c9584e60ed141bf64a1eb41
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-01-10 17:40:38 -0300

    Criação de modal Users e ajuste de pom.xml

---
+++ src/main/java/com/robsondejesus/todosimple/models/User.java
@@ -0,0 +1,93 @@
+package com.robsondejesus.todosimple.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+
+    public interface Createuser {
+    }
+
+    public interface Updateuser {
+    }
+
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = Createuser.class)
+    @NotEmpty(groups = Createuser.class)
+    @Size(groups = Createuser.class, min = 2, max = 100)
+    private String username;
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull(groups = { Createuser.class, Updateuser.class })
+    @NotEmpty(groups = { Createuser.class, Updateuser.class })
+    @Size(groups = { Createuser.class, Updateuser.class }, min = 8, max = 60)
+    private String password;
+
+    // private List<Task> asks = new ArrayList<Task>();
+
+    public User() {
+
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+}
