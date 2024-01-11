9190db0c997354b1da9ca0b61161993493ac17ac
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-01-11 15:21:56 -0300

    Criação do repository de Users

---
+++ src/main/java/com/robsondejesus/todosimple/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.robsondejesus.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.robsondejesus.todosimple.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+}
